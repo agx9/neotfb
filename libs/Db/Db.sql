1c93a14f22076020e6f41fbdb9fc86eae1d1970d
Anurag <anurag@xminds.in>
2012-10-14 15:49:40 +0530

    new db

--- libs/Db/Db.sql
+++ libs/Db/Db.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Community v9.50 
-MySQL - 5.1.58-1ubuntu1 : Database - ol_beats
+MySQL - 5.1.58-1ubuntu1 : Database - neo_fb
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 5.1.58-1ubuntu1 : Database - ol_beats
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`ol_beats` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`neo_fb` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `ol_beats`;
+USE `neo_fb`;
 
 /*Table structure for table `config` */
 
@@ -27,6 +27,10 @@ CREATE TABLE `config` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
+/*Data for the table `config` */
+
+insert  into `config`(`Id`,`ConfigKey`,`Value`) values (1,'APP_NAME','Ol'),(2,'APP_GMAIL',''),(3,'APP_GMAILPASSWORD',''),(4,'APP_SENDMAIL_POSTCOUNT','10'),(5,'APP_PAGE_NO_OF_ROWS_IN_TABLE','20'),(6,'APP_URL','http://dev.neofb.com'),(7,'APP_FB_APPID','329209187177028'),(8,'APP_RESOURCE_PATH','http://dev.neofb.com');
+
 /*Table structure for table `fb_friends` */
 
 DROP TABLE IF EXISTS `fb_friends`;
@@ -38,7 +42,9 @@ CREATE TABLE `fb_friends` (
   `Status` tinyint(2) DEFAULT NULL,
   `UserId` int(11) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `fb_friends` */
 
 /*Table structure for table `fb_groups` */
 
@@ -52,7 +58,9 @@ CREATE TABLE `fb_groups` (
   `Status` tinyint(2) DEFAULT NULL,
   `UserId` int(11) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `fb_groups` */
 
 /*Table structure for table `fb_posts` */
 
@@ -67,7 +75,9 @@ CREATE TABLE `fb_posts` (
   `Status` tinyint(2) DEFAULT NULL,
   `UserId` int(11) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `fb_posts` */
 
 /*Table structure for table `post_log` */
 
@@ -80,7 +90,9 @@ CREATE TABLE `post_log` (
   `UserId` int(11) DEFAULT NULL,
   `Status` tinyint(2) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `post_log` */
 
 /*Table structure for table `post_queue` */
 
@@ -94,7 +106,9 @@ CREATE TABLE `post_queue` (
   `Status` tinyint(2) DEFAULT NULL,
   `UserId` int(11) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `post_queue` */
 
 /*Table structure for table `tabs` */
 
@@ -105,7 +119,9 @@ CREATE TABLE `tabs` (
   `tabHtml` longtext,
   `pageId` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `tabs` */
 
 /*Table structure for table `user` */
 
@@ -118,7 +134,9 @@ CREATE TABLE `user` (
   `AccessToken` varchar(500) DEFAULT NULL,
   `Status` tinyint(2) DEFAULT NULL,
   PRIMARY KEY (`Id`,`FbUserId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
